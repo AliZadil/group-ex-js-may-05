0c19c0c9f73a6b6d17ebca86f40037d84163fc50
Ali Zadil <tarikz94@gmail.com>
2023-05-05 15:37:02 +0300

    done

--- main.js
+++ main.js
@@ -16,4 +16,70 @@ function convert(data, actionCb) {
     return actionCb(data);
 }
 
+function actionCallback(arr){
+    let { data } = JSON.parse(arr);
+    return data;
+}
+
 const convertedData = convert(jsonData, actionCallback);
+
+// console.log(convertedData);
+
+class DataProcess{
+    constructor(data){
+        this._data = data
+    }
+
+    sortData(){
+        this._data.sort((a,b) => a - b);
+    }
+    calculateTotal(){
+        this.total = this._data.reduce((a,b) => a + b);
+    }
+    isEven(){
+        return this.total % 2 === 0;
+    }
+    doDouble(){
+        this.double = this._data.map(a => a * 2);
+    }
+    static getOdd(arr){
+        return arr.filter(el => el % 2 === 1);
+    }
+    static getEven(arr){
+        return arr.filter(el => el % 2 === 0);
+    }
+    printProp(){
+        console.log(this._data);
+        console.log(this.total);
+        console.log(this.double);
+    }
+    toJSON(){
+        return {
+            data:this._data,
+            total:this.total,
+            double:this.double
+        }
+    }
+    
+}
+
+let classData = new DataProcess(convertedData);
+// console.log(classData);
+
+ classData.sortData();
+// console.log(classData);
+
+ classData.calculateTotal();
+// console.log(classData);
+
+// console.log(classData.isEven());
+
+ classData.doDouble();
+// console.log(classData);
+
+// console.log(DataProcess.getOdd(classData._data));
+// console.log(DataProcess.getEven(classData._data));
+
+// classData.printProp();
+
+console.log(JSON.stringify(classData))
